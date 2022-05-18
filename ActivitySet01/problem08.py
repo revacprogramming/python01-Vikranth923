047efa5afbb961cb653e654af28fb9c22a4e6030
Vikranth923 <vikranth923@gmail.com>
2022-05-18 15:16:46 +0530

    k

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,14 +1,12 @@
-# Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
+fname = "romeo.txt"
 fh = open(fname)
-sm = 0
-n = 0
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    else:
-        n=n+1
-        sm=sm+float(line.split()[1])
-avg=sm/n
-print("Average spam confidence:",avg)
+k=[]
+c=fh.readlines()
+for i in c:
+    j = i.split()
+    for i in j:
+        if i not in k:
+            k.append(i)
+            
 
+print(sorted(k))
